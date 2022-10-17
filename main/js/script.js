8882a7395fdbecfdaee352a160fbce123f457852
pavel11123 <korobko.11@inbox.ru>
2022-10-17 10:21:13 +0600

    first commit

---
+++ main/js/script.js
@@ -0,0 +1,7 @@
+function menu__open(){
+document.getElementById("phone__menu-id").style.width="100%";
+}
+
+function menu__close(){
+document.getElementById("phone__menu-id").style.width="0";
+}
---
+++ send.php
@@ -0,0 +1,42 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$product = $_POST['user_product'];
+$email = $_POST['user_email'];
+$text = $_POST['user_text'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'pavelpavlin123@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'fA*81ReuofTY'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('pavelpavlin123@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('korobko.11@inbox.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку <br> Почта этого пользователя: ' .$email. ' Телефон этого пользователя: ' .$phone . ' Наименование товара: ' .$product . '<br> message: ' .$text ;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: index.html');
+}
+?>
